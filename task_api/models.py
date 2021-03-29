6714beef7ee9d2fb85596dbfc29174a4fc4f73db
RLT <rellayt09s@gmail.com>
2021-03-29 16:19:41 +0200

    User model updated

--- task_api/models.py
+++ task_api/models.py
@@ -1,14 +1,14 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 # Create models
-class User(AbstractUser):
-    id = models.CharField(max_length=255, primary_key=True)
+class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
