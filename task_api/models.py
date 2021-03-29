c1c7f795a4d039fc5c7d9f49a1697702af1fd4de
Kamil Korbik <korbik.gm@gmail.com>
2021-03-29 17:27:30 +0200

    Added PersonalData and Address models

--- task_api/models.py
+++ task_api/models.py
@@ -2,12 +2,28 @@ import uuid
 
 from django.db import models
 
+
 # Create models
 class User(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    personal_data = models.ForeignKey('PersonalData', on_delete=models.CASCADE, null=True)
+    address = models.ForeignKey('Address', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class PersonalData(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=12)
+    birth_date = models.DateField()
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=60)
+    state = models.CharField(max_length=60)
+    country = models.CharField(max_length=60)
+    country_code = models.IntegerField()
